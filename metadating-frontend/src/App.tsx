7e782465d16cc1167bb8c22003e6d6a8c5ce8352
Krishna Dihora <kdihora@umich.edu>
2022-09-27 21:09:00 -0400

    Add example api call, use axios for requests

--- metadating-frontend/src/App.tsx
+++ metadating-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './Components/HelloWorld';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <HelloWorld/>
       </header>
     </div>
   );
