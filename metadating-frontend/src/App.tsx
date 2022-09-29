960c87d06eb0d351c5e4e5f30ad87a28bee64aa9
Krishna Dihora <kdihora@umich.edu>
2022-09-29 17:28:45 -0400

    Add tailwind css directives and create basic sidebar

--- metadating-frontend/src/App.tsx
+++ metadating-frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import FormPage from './Pages/FormPage';
+import SideBar from './Components/SideBar';
 
 function App() {
   return (
     <div className="App">
+        <div className='flex'>
+            <SideBar/>
+        </div>
         <FormPage />
     </div>
   );
