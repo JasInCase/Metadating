72a559abb05ad42287a8b9cab51445880aa1152a
vnaray <vnaray@umich.edu>
2022-11-24 17:26:53 -0500

    Update sign up page UI
    Make all inputs dynamic with proper eror messages and error colors. Make sign-up workflow log all provided data from frontend, backend, and database

--- metabackend/api/accounts.py
+++ metabackend/api/accounts.py
@@ -85,11 +85,13 @@ def do_create():
     if 'username' not in flask.request.json or 'password' not in flask.request.json:
         flask.abort(400)
 
+    name = flask.request.json['name']
+    email = flask.request.json['email']
     username = flask.request.json['username']
     pwd = flask.request.json['password']
 
     # Error checking
-    if not username or not pwd:
+    if not name or not email or not username or not pwd:
         flask.abort(400)
 
     # Make sure the user doesn't already exist
@@ -103,7 +105,7 @@ def do_create():
     stored_pwd = '$'.join([algo, salt, hashed_pwd])
 
     # Add the user to the database
-    response = insert_user(username, stored_pwd)
+    response = insert_user(name, email, username, stored_pwd)
     new_user_id = str(response.inserted_id)
 
     if response.acknowledged:
--- metadating-frontend/src/Pages/SignUpPage.tsx
+++ metadating-frontend/src/Pages/SignUpPage.tsx
@@ -15,31 +15,18 @@ export async function getMessageFromAPI() {
 }
 
 
-const sendLoginToAPI = async (username: string, password: string) => {
+type signUpData = {
+    name: string,
+    email: string,
+    username: string,
+    password: string
+};
+
+
+const sendSignupToAPI = async (data: signUpData) => {
+    const dataToSend = {...data, operation: 'create'};
     const response = await axios.post('/api/v1/accounts/',
-        {
-            username: username,
-            password: password,
-            operation: "login"
-        },
-        {
-            headers: {
-                'content-type': 'application/json'
-            }
-        }
-    );
-
-    return response;
-}
-
-
-const sendSignupToAPI = async (username: string, password: string) => {
-    const response = await axios.post('/api/v1/accounts/',
-        {
-            username: username,
-            password: password,
-            operation: "create"
-        },
+        dataToSend,
         {
             headers: {
                 'content-type': 'application/json'
@@ -53,14 +40,45 @@ const sendSignupToAPI = async (username: string, password: string) => {
 
 const SignUpPage = () => {
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [validName, setValidName] = useState(true);
+    const [validEmail, setValidEmail] = useState(true);
     const [validUsername, setValidUsername] = useState(true);
     const [validPassword, setValidPassword] = useState(true);
+    const [validConfirmPassword, setValidConfirmPassword] = useState(true);
+    const [errorTextName, setErrorTextName] = useState('');
+    const [errorTextEmail, setErrorTextEmail] = useState('');
     const [errorTextUsername, setErrorTextUsername] = useState('');
     const [errorTextPassword, setErrorTextPassword] = useState('');
+    const [errorTextConfirmPassword, setErrorTextConfirmPassword] = useState('');
     const [invalidCredentials, setInvalidCredentials] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
+
+    const onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const enteredName = event.target.value;
+      setName(enteredName);
+
+      // Validate input
+      if (!validName && event.target.value !== '') {
+          setValidName(true);
+          setErrorTextName('');
+      }
+    }
+
+    const onEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const enteredEmail = event.target.value;
+      setEmail(enteredEmail);
+
+      // Validate input
+      if (!validEmail && event.target.value !== '') {
+          setValidEmail(true);
+          setErrorTextEmail('');
+      }
+    }
 
     const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const enteredUsername = event.target.value;
@@ -80,79 +98,77 @@ const SignUpPage = () => {
         // Validate input
         if (!validPassword && event.target.value !== '') {
             setValidPassword(true);
-            setErrorTextUsername('');
+            setErrorTextPassword('');
         }
     }
 
-    const submitLogIn = (event: any) => {
-        if (event) {
-            event.preventDefault();
-        }
+    const onConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const enteredConfirmPassword = event.target.value;
+        setConfirmPassword(enteredConfirmPassword);
 
-        // Check for empty fields
-        let emptyFields = false;
-        if (!username || !username.trim()) {
-            setValidUsername(false);
-            setErrorTextUsername('Please input a valid username.');
-            emptyFields = true;
+        // Validate input
+        if (!validConfirmPassword && event.target.value !== '') {
+            setValidConfirmPassword(true);
+            setErrorTextConfirmPassword('');
         }
-        if (!password || !password.trim()) {
-            setValidPassword(false);
-            setErrorTextPassword('Please input a valid password.');
-            emptyFields = true;
-        }
-        if (emptyFields) {
-            return;
-        }
-
-        // Show loading icon and check login
-        setIsLoading(true);
-
-        sendLoginToAPI(username, password).then((res) => {
-            // console.log(res)
-            window.localStorage.setItem("username", res.data.username);
-            window.localStorage.setItem("userId", res.data.userId);
-            window.location.assign('/form');
-
-        })
-            .catch(err => {
-                setInvalidCredentials(true);
-                setIsLoading(false);
-                console.error(err);
-            });
     }
 
     const submitSignUp = () => {
         // Check for empty fields
         let emptyFields = false;
-        if (!username || !username.trim()) {
+        if (!name.trim()) {
+            setValidName(false);
+            setErrorTextName('Plase input a valid name.');
+            emptyFields = true;
+        }
+        if (!email.trim()) {
+            setValidEmail(false);
+            setErrorTextEmail('Plase input a valid email.');
+            emptyFields = true;
+        }
+        if (!username.trim()) {
             setValidUsername(false);
-            setErrorTextUsername("Please input a valid username.");
+            setErrorTextUsername('Please input a valid username.');
             emptyFields = true;
         }
-        if (!password || !password.trim()) {
+
+        if (!password.trim()) {
             setValidPassword(false);
-            setErrorTextPassword("Please input a valid password.");
+            setErrorTextPassword('Please input a valid password.');
             emptyFields = true;
         }
+        else if (password !== confirmPassword) {
+            setValidConfirmPassword(false);
+            setErrorTextConfirmPassword('Passwords do not match.');
+            emptyFields = true;
+        }
+
         if (emptyFields) {
             return;
         }
 
         // Show loading icon and sign up
         setIsLoading(true);
-        sendSignupToAPI(username, password).then((res) => {
+
+        const data: signUpData = {
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        };
+
+        sendSignupToAPI(data).then((res) => {
 
             window.localStorage.setItem("username", res.data.username);
             window.localStorage.setItem("userId", res.data.userId);
             window.location.assign('/form');
 
         })
-            .catch(err => {
-                setInvalidCredentials(true);
-                setIsLoading(false);
-                console.error(err);
-            });
+        .catch(err => {
+            setInvalidCredentials(true);
+            setIsLoading(false);
+            console.error(err);
+        });
     }
 
     const navigateToLoginPage = (event: any) => {
@@ -171,15 +187,37 @@ const SignUpPage = () => {
           <div className="pt-3 pb-2">
             <div className="p-2">
               <FormControl fullWidth={true}>
-                <InputLabel htmlFor="username"> Name </InputLabel>
-                <OutlinedInput fullWidth id="username" label="Username" />
+                <InputLabel htmlFor="name"> Name </InputLabel>
+                <OutlinedInput
+                  error={!validName}
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  autoComplete="on"
+                  value={name}
+                  onChange={onNameChange}
+                />
+                {!validName && (
+                  <FormHelperText>{errorTextName}</FormHelperText>
+                )}
               </FormControl>
             </div>
 
             <div className="p-2">
               <FormControl fullWidth={true}>
-                <InputLabel htmlFor="username"> Email </InputLabel>
-                <OutlinedInput fullWidth id="username" label="Username" />
+                <InputLabel htmlFor="email"> Email </InputLabel>
+                <OutlinedInput
+                  error={!validEmail}
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  autoComplete="on"
+                  value={email}
+                  onChange={onEmailChange}
+                />
+                {!validEmail && (
+                  <FormHelperText>{errorTextEmail}</FormHelperText>
+                )}
               </FormControl>
             </div>
 
@@ -201,30 +239,40 @@ const SignUpPage = () => {
             </div>
 
             <div className="p-2">
-              <form onSubmit={submitLogIn}>
-                <FormControl fullWidth={true}>
-                  <InputLabel htmlFor="name"> Password </InputLabel>
-                  <OutlinedInput
-                    error={!validPassword}
-                    fullWidth
-                    id="name"
-                    type="password"
-                    label="Password"
-                    autoComplete="on"
-                    value={password}
-                    onChange={onPasswordChange}
-                  />
-                  {!validPassword && (
-                    <FormHelperText>{errorTextPassword}</FormHelperText>
-                  )}
-                </FormControl>
-              </form>
+              <FormControl fullWidth={true}>
+                <InputLabel htmlFor="name"> Password </InputLabel>
+                <OutlinedInput
+                  error={!validPassword}
+                  fullWidth
+                  id="password"
+                  type="password"
+                  label="Password"
+                  autoComplete="off"
+                  value={password}
+                  onChange={onPasswordChange}
+                />
+                {!validPassword && (
+                  <FormHelperText>{errorTextPassword}</FormHelperText>
+                )}
+              </FormControl>
             </div>
 
             <div className="p-2">
               <FormControl fullWidth={true}>
                 <InputLabel htmlFor="username"> Confirm Password </InputLabel>
-                <OutlinedInput fullWidth id="username" label="Username" />
+                <OutlinedInput
+                  error={!validConfirmPassword}
+                  fullWidth
+                  id="confirm-password"
+                  type="password"
+                  label="Confirm assword"
+                  autoComplete="off"
+                  value={confirmPassword}
+                  onChange={onConfirmPasswordChange}
+                />
+                {!validConfirmPassword && (
+                  <FormHelperText>{errorTextConfirmPassword}</FormHelperText>
+                )}
               </FormControl>
             </div>
 
